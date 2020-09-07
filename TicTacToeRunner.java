8c8600f787753074355321aa0dba640bfae128c0
joshab13 <59944943+joshab13@users.noreply.github.com>
2020-09-07 17:20:51 -0400

    Update TicTacToeRunner.java
    Added comments

--- TicTacToeRunner.java
+++ TicTacToeRunner.java
@@ -9,22 +9,24 @@ public class TicTacToeRunner {
 	
 	public TicTacToeRunner() {
 		
-	boolean running = true;
-	
+	boolean running = true; /*The running variable is used to control flow of the program.
+	It will remain true until the user decides to stop playing the game.
+	*/
 	
 	while(running) {
 		
 		program = new TicTacToe(); /*Creates a new instance of the program every time a player
-		wants to play the game again.
+		wants to play the game again. This is where the actual Tic-Tac-Toe code lies
 		 */
 		
 		
-		while(true) {
+		while(true) { //Keeps asking question to user until they provide exact input or they enter "No"
 			
 			System.out.println("Do you want to play again? Please say Yes or No:");
-			Scanner playagain = new Scanner(System.in);
+			Scanner playagain = new Scanner(System.in); //Scanner created to gather input from user
 			String test = playagain.next();
-			System.out.println(test);
+			
+			//System.out.println(test);
 			
 			if(!(test.equals("Yes") || test.equals("No"))){
 			
@@ -33,7 +35,7 @@ public class TicTacToeRunner {
 			}
 			
 			if(test.equals("No")) {
-				running = false;	
+				running = false;	//This will ensure the program doesn't again
 				break;
 				}
 			
